61914a671c9ee7ac70242b12971a8f3966a64948
Ramla Abdullahi <ramlaali.abd@gmail.com>
2021-06-11 22:23:23 -0400

    commit tree 1563b2ba753910b77196e2698ff114977b688ad2

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(){
+    const division = 2000 / 100;
+    return division; 
+}
+
+const square = (parameter1) => parameter1 * parameter1;
+
+const add = (para1, para2) => para1 +para2;
